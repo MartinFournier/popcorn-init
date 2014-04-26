6a13fa9837656de90149f3c75ddf1f27a4eb7aa8
Martin Fournier <martinfournier@scolab.com>
2014-04-26 11:40:13 -0400

    Add barebone model for combat turns tracking.

---
+++ src/entities/combat/combatModel.js
@@ -0,0 +1,12 @@
+﻿define([
+    'underscore',
+    'backbone'
+], function (_, Backbone) {
+    var CombatModel = Backbone.Model.extend({
+        defaults: {
+            turn: 0
+        }
+    });
+
+    return CombatModel;
+});
